7ca6d07a2a471d53ee5098ef77f81025fb0cea61
Chelsea Ball <chelseaball@Chelseas-MacBook-Air.local>
2021-04-02 13:23:58 -0700

    Select all sample names for dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,21 @@
 function init(){
-    alert("hello world")
+    console.log("hello world");
+
+    //Reference the dropdown id selDataset
+    var selector = d3.select("#selDataset");
+    
+    //GEt sample names for dropdown
+    d3.json("samples.json").then(function(data){
+      //console.log(data);
+      var sampleNames = data.names;
+
+      sampleNames.forEach(function(name){
+          selector
+          .append("option")
+          .text(name)
+          .property("value", name)
+      })
+    })
 }
 
 init()
